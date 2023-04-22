d576fa99236147cdefc5b7765c1b5796aa29f511
请问墙和柱子能奶你吗 <zhu.thatboy@outlook.com>
2023-04-22 23:35:26 +0800

    Update version.test.cpp

--- test/version.test.cpp
+++ test/version.test.cpp
@@ -3,5 +3,12 @@
 
 TEST_CASE("Version info not empty", "[single-file]" ) {
     REQUIRE(VersionHelper::getInstance().AppName != "");
+    REQUIRE(VersionHelper::getInstance().Author != "");
+    REQUIRE(VersionHelper::getInstance().Email != "");
+    REQUIRE(VersionHelper::getInstance().Description != "");
+    REQUIRE(VersionHelper::getInstance().Version != "");
     REQUIRE(VersionHelper::getInstance().CommitHash != "");
+    REQUIRE(VersionHelper::getInstance().CommitDate != "");
+    REQUIRE(VersionHelper::getInstance().BuildDate != "");
+    REQUIRE(VersionHelper::getInstance().BuildTime != "");
 }
