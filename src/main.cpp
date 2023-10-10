e04a72d3d3be1d3116272c0429fe8d967ff51c26
Choo <zhu.thatboy@outlook.com>
2023-10-10 09:00:19 +0000

    Add cmake modules
    - Add cmake modules
    - Optmise build cmake

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,10 @@ int main() {
     std::cout << "Author: " << VersionHelper::getInstance().Author << std::endl;
     std::cout << "Email: " << VersionHelper::getInstance().Email << std::endl;
     std::cout << "Version: " << VersionHelper::getInstance().Version << std::endl;
-    std::cout << "CommitHash: " << VersionHelper::getInstance().CommitHash << std::endl;
-    std::cout << "CommitDate: " << VersionHelper::getInstance().CommitDate << std::endl;
+    std::cout << "GitDescribe: " << VersionHelper::getInstance().GitDescribe << std::endl;
+    std::cout << "GitSha1: " << VersionHelper::getInstance().GitSha1 << std::endl;
+    std::cout << "GitRefSpec: " << VersionHelper::getInstance().GitRefSpec << std::endl;
     std::cout << "BuildDate: " << VersionHelper::getInstance().BuildDate << std::endl;
     std::cout << "BuildTime: " << VersionHelper::getInstance().BuildTime << std::endl;
-    std::cout << "IsDevVersion: " << VersionHelper::getInstance().IsDevVersion << std::endl;
     return 0;
 }
